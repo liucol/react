bcc8336472b2916857bac04ac997e614087eda31
liucol <1545766219@qq.com>
2017-09-21 14:44:05 +0800

    引入jquery

--- webpack-demo/app/index.js
+++ webpack-demo/app/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
+import j from 'jquery'
 function component () {
-    var element = document.createElement('div');
+    var element=j('<div></div>')
 
     /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-    return element;
+    element.html(_.join(['Hello','webpack','啦啦啦'], ' '))
+    return element.get(0);
 }
 
 document.body.appendChild(component());
