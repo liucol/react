8897f91c6d3e46db99cba2cee47342148e86b67e
liucol <1545766219@qq.com>
2017-09-22 14:05:22 +0800

    按需更新first例子

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <h1>阿哈，Hello, world!啦啦啦</h1>,
-    document.getElementById('root2')
-);
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+}
+
+setInterval(tick, 1000);
 
