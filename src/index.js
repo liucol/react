02dfe96c8a8b7f2a7934e5239dbb4b47f14f4340
liucol <1545766219@qq.com>
2017-09-22 10:17:14 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <h1>阿哈，Hello, world!啦啦啦</h1>,
+    document.getElementById('root2')
+);
+
