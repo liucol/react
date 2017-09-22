990ccf2f475d2c356efc5a94ff6aee8374dbc5df
liucol <1545766219@qq.com>
2017-09-22 14:16:12 +0800

    把组件放到一个单独的文件里面

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/9/22 0022.
+ */
+import React from 'react';
+
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, Component,FIRST</h1>;
+    }
+}
+export default  Welcome
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Welcome from './Welcome';
 import './index.css';
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
-
+ReactDOM.render(
+    <Welcome/>,
+    document.getElementById('root')
+)
